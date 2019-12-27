00b621b2928dcef55305301e0c3f7b092fb2b9bc
362279869@qq.com <362279869@qq.com>
2019-12-27 17:20:53 +0800

    init

---
+++ src/watcher.js
@@ -0,0 +1,132 @@
+// MIT License
+//
+// Copyright(c) 2019 yunmin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// 	The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const utils = require('../utils/utils');
+const EventEmitter = require('events').EventEmitter;
+
+let instance = null;
+
+module.exports = function (opts) {
+    // singleton
+    if (instance) {
+        return instance;
+    }
+    // create component
+    instance = new Component(opts);
+    return instance;
+};
+
+function fixPath(dir) {
+    if (dir &&
+        dir.charAt(dir.length - 1) !== '/') {
+        dir += '/';
+    }
+    return dir;
+}
+
+function loadAll(cmp) {
+    let files = [];
+    utils.readFiles(cmp.dir, files, true);
+    files.forEach(function (file) {
+        if (!/\.json$/.test(file.name)) {
+            console.warn('not a json file: ' + file.name);
+            return;
+        }
+        let k_name = file.name.split('.')[0];
+        if (cmp.dataTbl[k_name] !== undefined) {
+            console.warn('duplicate file: ' + file.name);
+            return;
+        }
+        let fullpath = path.join(file.path, file.name);
+        let data = utils.loadFileSync(fullpath);
+        if (data) {
+            cmp.dataTbl[k_name] = data;
+            fs.watchFile(fullpath, {
+                persistent: true,
+                interval: cmp.interval
+            }, cmp.listener4watch(fullpath, k_name));
+            utils.invokeCallback(cmp.onLoaded, k_name);
+        }
+    });
+    utils.invokeCallback(cmp.onAllLoaded);
+}
+
+let Component = function (opts) {
+    // config option
+    this.dir = fixPath(opts.dir);
+    this.interval = opts.interval || 5000;
+    // json table{name=>json}
+    this.dataTbl = {};
+    // the file is loaded
+    this.onLoaded = opts.onLoaded;
+    // all loaded up
+    this.onAllLoaded = opts.onAllLoaded;
+    // check if dir exist
+    if (!fs.existsSync(this.dir)) {
+        console.error('dir %s not exist!', this.dir);
+    }
+    loadAll(this);
+};
+
+util.inherits(Component, EventEmitter);
+
+Component.prototype.stop = function (force, cb) {
+    this.dataTbl = null;
+    utils.invokeCallback(cb);
+};
+
+Component.prototype.loadFileFunc = function (filename, key) {
+    let self = this;
+    utils.loadFile(filename, function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        if (data) {
+            self.dataTbl[key] = data;
+            console.log("reloaded: " + filename);
+        } else {
+            console.error("fail to reloaded: " + filename);
+        }
+    });
+};
+
+Component.prototype.listener4watch = function (filename, key) {
+    let self = this;
+    return function (curr, prev) {
+        if (curr.mtime.getTime() > prev.mtime.getTime()) {
+            self.loadFileFunc(filename, key);
+        }
+    };
+};
+
+Component.prototype.get = function (name) {
+    return this.dataTbl[name];
+};
+
+Component.prototype.has = function (name) {
+    return this.dataTbl[name] !== undefined;
+};
+
---
+++ utils/utils.js
@@ -0,0 +1,80 @@
+// MIT License
+//
+// Copyright(c) 2019 yunmin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// 	The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const fs = require('fs');
+
+let Utils = module.exports;
+
+// invoke a function
+Utils.invokeCallback = function (cb) {
+    if (!!cb && typeof cb === 'function') {
+        cb.apply(null, Array.prototype.slice.call(arguments, 1));
+    }
+};
+
+// load a file async
+Utils.loadFile = function (path, cb) {
+    fs.readFile(path, function (err, stream) {
+        if (err) {
+            Utils.invokeCallback(cb, err);
+            return;
+        }
+        let rt = null;
+        try {
+            rt = JSON.parse(stream);
+        } catch (e) {
+            console.error(path);
+        }
+        Utils.invokeCallback(cb, null, rt);
+    });
+};
+
+// load a file sync
+Utils.loadFileSync = function (path) {
+    let stream = fs.readFileSync(path);
+    let rt = null;
+    try {
+        rt = JSON.parse(stream);
+    } catch (e) {
+        console.error(path);
+    }
+    return rt;
+};
+
+// read all files from a dir
+Utils.readFiles = function (path, fileArr, recursion) {
+    let files = fs.readdirSync(path);
+    files.forEach(function (itm, index) {
+        let stat = fs.statSync(path + itm);
+        if (stat.isDirectory()) {
+            if (recursion) {
+                Utils.readFiles(path + itm + "/", fileArr, recursion);
+            }
+        } else {
+            let obj = {};
+            obj.path = path;
+            obj.name = itm;
+            fileArr.push(obj);
+        }
+    });
+};
+
